96156799717b7ed7f1efd816785c89e5863b821e
darksun850 <mfarooqui@3clogic.com>
2022-09-05 13:02:02 +0530

    Get Properties for delivery

--- src/main/java/com/ccclogic/sailor/repositories/metadata/MetadataPropertiesRepository.java
+++ src/main/java/com/ccclogic/sailor/repositories/metadata/MetadataPropertiesRepository.java
@@ -2,6 +2,7 @@ package com.ccclogic.sailor.repositories.metadata;
 
 import com.ccclogic.sailor.entities.metadata.MetadataProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface MetadataPropertiesRepository extends JpaRepository<MetadataProperties, Long> {
     
     List<MetadataProperties> findByLoadOn(Integer loadOn);
+
+    @Query(value = "select * from  properties where `key` in (:props)", nativeQuery = true)
+    List<MetadataProperties> findPropForSynergy(List<String> props);
 
 }
---
+++ src/main/java/com/ccclogic/sailor/services/metadata/MetadataPropertiesService.java
@@ -0,0 +1,11 @@
+package com.ccclogic.sailor.services.metadata;
+
+import java.util.Map;
+
+public interface MetadataPropertiesService {
+
+    Map<String, String> getAllProperties();
+
+
+    Map<String, String> getPropForSynergy();
+}
