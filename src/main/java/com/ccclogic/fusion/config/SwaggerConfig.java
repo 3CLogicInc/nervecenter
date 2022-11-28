71fddec424cb93fda7c8a17473422cff63818254
Devendra <dtiwari18@gmail.com>
2022-11-28 17:10:54 +0530

    Updated Swagger config

--- src/main/java/com/ccclogic/fusion/config/SwaggerConfig.java
+++ src/main/java/com/ccclogic/fusion/config/SwaggerConfig.java
@@ -1,6 +1,10 @@
 package com.ccclogic.fusion.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +12,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    public static final String BEARER_AUTH = "BEARER_AUTH";
+
     @Bean
-    public OperationCustomizer operationCustomizer(){
+    public OperationCustomizer operationCustomizer() {
         return ((operation, handlerMethod) -> {
-            operation.addParametersItem(
-                    new Parameter()
-                            .in("header")
-                            .required(false)
-                            .name("Authorization"));
             operation.addParametersItem(
                     new Parameter()
                             .in("path")
@@ -24,5 +25,18 @@ public class SwaggerConfig {
 
             return operation;
         });
+    }
+
+    @Bean
+    public OpenAPI customiseOpenApi() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(BEARER_AUTH))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .name(BEARER_AUTH)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
