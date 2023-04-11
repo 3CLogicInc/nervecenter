632fce5e0647bf0b09b10a58cca8ea442a5e198f
Devendra <dtiwari18@gmail.com>
2023-04-11 13:16:38 +0530

    Updated webastra.sql

--- src/main/resources/scripts/webastra.sql
+++ src/main/resources/scripts/webastra.sql
@@ -1,38 +1,38 @@
-create table nc_routes
+create table webastra.nc_routes
 (
-    id         int auto_increment,
-    name       varchar(255) null,
-    domain     varchar(512) null,
-    is_default boolean   default false null,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name varchar(255) CHAR SET 'utf8mb4' NOT NULL DEFAULT '',
+    domain varchar(512) null,
+    is_default boolean default false null,
     created_at timestamp default CURRENT_TIMESTAMP null,
-    updated_at timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP () null,
-    created_by int null,
-    updated_by int null,
+    updated_at timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP() null,
+    created_by int UNSIGNED null,
+    updated_by int UNSIGNED null,
     constraint nc_routes_pk
         primary key (id)
 );
 
 create table webastra.nc_route_exceptions
 (
-    id                  int                                 not null auto_increment,
-    route_id            int                                 not null,
-    country             varchar(64) null,
-    prefix              int null,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    route_id int UNSIGNED not null,
+    country varchar(64) null,
+    prefix int null,
     exception_domain_id int null,
-    created_at          timestamp default CURRENT_TIMESTAMP not null,
-    updated_at          timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
-    created_by          int null,
-    updated_by          int null,
-    primary key (id)
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    updated_at timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    created_by int UNSIGNED null,
+    updated_by int UNSIGNED null,
+    primary key (id),
+    unique key (id, route_id)
 );
 
 create table webastra.nc_callcenter_routes
 (
-    callcenter_id int not null,
-    route_id      int not null,
-    created_at    timestamp default current_timestamp null,
-    created_by    int null,
+    callcenter_id int UNSIGNED not null,
+    route_id int UNSIGNED not null,
+    created_at timestamp default current_timestamp null,
+    created_by int null,
     constraint nc_callcenter_routes_pk
         unique (callcenter_id, route_id)
 );
-
