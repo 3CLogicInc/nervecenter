c29f7eb36dfeb7d6ebd63f86dfd369d8f5c4e712
sumit <sumitchopra16@gmail.com>
2024-02-23 10:37:10 +0530

    PS-6205

--- src/main/resources/scripts/webastra.sql
+++ src/main/resources/scripts/webastra.sql
@@ -42,3 +42,96 @@ CREATE TABLE webastra.`nc_domains` (
                               PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`role` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) CHARACTER SET 'utf8mb4' NOT NULL DEFAULT '',
+	`hierarchy` INT(10) NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    `updated_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_name (`name`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`permission` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) CHARACTER SET 'utf8mb4' NOT NULL DEFAULT '',
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    `updated_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_name (`name`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`role_permission` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `role_id` INT UNSIGNED NOT NULL,
+	`permission_id` INT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_role_id_permission_id (`role_id`,`permission_id`),
+	CONSTRAINT `role_permission_fk_1` FOREIGN KEY (`role_id`) REFERENCES `!-TENANT-!`.`role` (`id`),
+	CONSTRAINT `role_permission_fk_2` FOREIGN KEY (`permission_id`) REFERENCES `!-TENANT-!`.`permission` (`id`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`user_role` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT UNSIGNED NOT NULL,
+	`role_id` INT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_user_id_role_id (`user_id`, `role_id`),
+	CONSTRAINT `user_role_fk_1` FOREIGN KEY (`user_id`) REFERENCES `!-TENANT-!`.`user` (`id`),
+	CONSTRAINT `user_role_fk_2` FOREIGN KEY (`role_id`) REFERENCES `!-TENANT-!`.`role` (`id`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`license` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) CHARACTER SET 'utf8mb4' NOT NULL,
+	`type` VARCHAR(256) CHARACTER SET 'utf8mb4' NOT NULL,
+	`quantity` INT UNSIGNED NOT NULL,
+	`start_date_time` INT UNSIGNED NOT NULL,
+	`end_date_time` INT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    `updated_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_name (`name`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`license_permission` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `license_id` INT UNSIGNED NOT NULL,
+	`permission_id` INT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_license_id_permission_id (`license_id`, `permission_id`),
+	CONSTRAINT `license_permission_fk_1` FOREIGN KEY (`license_id`) REFERENCES `!-TENANT-!`.`license` (`id`),
+	CONSTRAINT `license_permission_fk_2` FOREIGN KEY (`permission_id`) REFERENCES `!-TENANT-!`.`permission` (`id`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS `!-TENANT-!`.`account_license` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`account_id` INT UNSIGNED NOT NULL,
+    `license_id` INT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` INT UNSIGNED NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY idx_account_id_license_id (`account_id`, `license_id`),
+	CONSTRAINT `account_license_fk_2` FOREIGN KEY (`account_id`) REFERENCES `!-TENANT-!`.`accounts` (`id`),
+	CONSTRAINT `account_license_fk_1` FOREIGN KEY (`license_id`) REFERENCES `!-TENANT-!`.`license` (`id`)
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = latin1;
