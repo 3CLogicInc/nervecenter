78bab7d063dc651708ea5455a0889dbafd6e9df0
ssrivastava <ssrivastava@3clogic.com>
2024-03-11 13:55:17 +0530

    code added for PS-6208

--- src/main/resources/scripts/webastra.sql
+++ src/main/resources/scripts/webastra.sql
@@ -1,7 +1,3 @@
--- noinspection SqlDialectInspectionForFile
-
--- noinspection SqlNoDataSourceInspectionForFile
-
 CREATE TABLE `nc_routes` (
                              `id` int unsigned NOT NULL AUTO_INCREMENT,
                              `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
